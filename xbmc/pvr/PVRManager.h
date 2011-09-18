1d689f8657d8146ca07f5c6d3f2cb92bcad877a3
Jonatan Antoni <jonatan@familie-antoni.de>
2011-09-18 20:13:15 +0200

    Moved pvr idle calculation to PVRManager class to simplyfy activity timer calculation within Application class. PVRManager calls configurable set wake up command on stop to wake up the box for the next event. Enhanced wake up with daily wake up. Daily wake is used to bring up the box once a day when no timers are active, to refresh the epg for instance.

--- xbmc/pvr/PVRManager.h
+++ xbmc/pvr/PVRManager.h
@@ -259,6 +259,12 @@ namespace PVR
     bool IsRecording(void) const;
 
     /*!
+     * @brief Check whether the pvr backend is idle.
+     * @return True if there are no active timers/recordings/wake-ups within the configured time span.
+     */
+    bool IsIdle(void) const;
+
+    /*!
      * @brief Set the current playing group, used to load the right channel.
      * @param group The new group.
      */
@@ -440,6 +446,11 @@ namespace PVR
      * @brief Update all timers.
      */
     void UpdateTimers(void);
+
+    /*!
+     * @brief Calculate next event time (timer or daily wake up)
+     */
+    CDateTime CalcNextEventTime(void) const;
 
     /*!
      * @brief Update all channels.
