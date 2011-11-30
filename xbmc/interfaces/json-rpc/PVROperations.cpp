8725d11c42146ee2ab0c32d67b5cbb78e369135b
FSS <fss@karburator.(none)>
2011-11-30 16:42:02 +0100

    Add missing json files

---
+++ xbmc/interfaces/json-rpc/PVROperations.cpp
@@ -0,0 +1,98 @@
+/*
+ *
+ */
+
+#include "PVROperations.h"
+#include "Application.h"
+#include "utils/log.h"
+
+#include "pvr/PVRManager.h"
+#include "pvr/channels/PVRChannelGroupsContainer.h"
+#include "pvr/channels/PVRChannel.h"
+#include "pvr/timers/PVRTimers.h"
+#include "pvr/timers/PVRTimerInfoTag.h"
+#include "epg/EpgInfoTag.h"
+
+using namespace JSONRPC;
+using namespace PVR;
+using namespace EPG;
+
+JSON_STATUS CPVROperations::ChannelSwitch(const CStdString &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result)
+{
+  unsigned int iChannelId = (unsigned int) parameterObject["channelid"].asUnsignedInteger();
+
+  CLog::Log(LOGINFO, "JSON PVR:Switch channel: %d", iChannelId);
+
+  if ( g_PVRManager.IsStarted() )
+  {
+    const CPVRChannel *channel = g_PVRManager.ChannelGroups()->GetByChannelIDFromAll(iChannelId);
+    if ( g_PVRManager.StartPlayback(channel, false) )
+      return OK;
+    else
+      return InternalError;
+  }
+  else
+  {
+    CLog::Log(LOGINFO, "JSON PVR failed to Switch channels. PVR not started");
+    return FailedToExecute;
+  }
+
+  return OK;
+}
+
+JSON_STATUS CPVROperations::ChannelUp(const CStdString &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result)
+{
+  CLog::Log(LOGINFO, "PVR: channel up");
+
+  if ( g_PVRManager.IsStarted() && g_PVRManager.IsPlaying() && g_application.m_pPlayer )
+  {
+    unsigned int iNewChannelNumber(0);
+    g_PVRManager.ChannelUp( &iNewChannelNumber );
+
+    CLog::Log(LOGINFO, "PVR: new channel %d", iNewChannelNumber);
+  }
+  return OK;
+}
+
+JSON_STATUS CPVROperations::ChannelDown(const CStdString &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result)
+{
+  CLog::Log(LOGINFO, "PVR: channel down");
+
+  if ( g_PVRManager.IsStarted() && g_PVRManager.IsPlaying() && g_application.m_pPlayer )
+  {
+    unsigned int iNewChannelNumber(0);
+    g_PVRManager.ChannelDown( &iNewChannelNumber );
+
+    CLog::Log(LOGINFO, "PVR: new channel %d", iNewChannelNumber);
+  }
+
+  return OK;
+}
+
+JSON_STATUS CPVROperations::ScheduleRecording(const CStdString &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result){
+
+  if ( g_PVRManager.IsStarted() ) {
+
+    unsigned int iChannelId = (unsigned int) parameterObject["channelid"].asUnsignedInteger();
+    unsigned int iUniquetId = (unsigned int) parameterObject["uniqueid"].asUnsignedInteger();
+    unsigned int iStartTime = (unsigned int) parameterObject["starttime"].asUnsignedInteger();
+
+    CPVRChannelGroupsContainer *CGC = g_PVRManager.ChannelGroups();
+    const CPVRChannel *channel = CGC->GetByChannelIDFromAll(iChannelId);
+
+    CDateTime *startTime = new CDateTime( iStartTime );
+    CEpgInfoTag *tag = channel->GetEPG()->GetTag(iUniquetId, *startTime );
+    delete startTime;
+
+    if ( tag ){
+      CPVRTimerInfoTag *newTimer = CPVRTimerInfoTag::CreateFromEpg(*tag);
+      bool bReturn = CPVRTimers::AddTimer(*newTimer);
+
+      CLog::Log(LOGINFO, "PVR: record result %d", bReturn);
+
+      delete newTimer;
+    }
+  }
+
+  return OK;
+}
---
+++ xbmc/interfaces/json-rpc/PVROperations.h
@@ -0,0 +1,20 @@
+#pragma once
+/*
+ *
+ */
+
+#include "utils/StdString.h"
+#include "JSONRPC.h"
+
+namespace JSONRPC
+{
+  class CPVROperations
+  {
+  public:
+    static JSON_STATUS ChannelSwitch(const CStdString &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
+    static JSON_STATUS ChannelUp(const CStdString &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
+    static JSON_STATUS ChannelDown(const CStdString &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
+    static JSON_STATUS ScheduleRecording(const CStdString &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
+  private:
+  };
+}
