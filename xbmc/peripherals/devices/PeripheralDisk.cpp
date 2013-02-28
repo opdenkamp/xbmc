317ac5bbefe40a7dcd7a599316dbe587ba29cf73
Lars Op den Kamp <lars@opdenkamp.eu>
2013-02-28 11:09:25 +0100

    [cec] rebased PR1794 for Frodo

--- xbmc/peripherals/devices/PeripheralDisk.cpp
+++ xbmc/peripherals/devices/PeripheralDisk.cpp
@@ -23,8 +23,8 @@
 
 using namespace PERIPHERALS;
 
-CPeripheralDisk::CPeripheralDisk(const PeripheralType type, const PeripheralBusType busType, const CStdString &strLocation, const CStdString &strDeviceName, int iVendorId, int iProductId) :
-  CPeripheral(type, busType, strLocation, strDeviceName.IsEmpty() ? g_localizeStrings.Get(35003) : strDeviceName, iVendorId, iProductId)
+CPeripheralDisk::CPeripheralDisk(const PeripheralScanResult& scanResult) :
+  CPeripheral(scanResult)
 {
   m_features.push_back(FEATURE_DISK);
 }
